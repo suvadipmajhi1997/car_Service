c97f2bfa3dbd06e239fca957b65663cb64d04791
suvadip majhi <114084926+suvadipmajhi1997@users.noreply.github.com>
2023-08-28 09:59:41 +0530

    1.0

---
+++ src/CarService.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public interface CarService {
+
+
+    HashMap<String, Pair> getServicePrices();
+
+
+    public int basicService();
+
+    public int engineFixing();
+
+    public int clutchFixing();
+
+    public int brakeFixing();
+
+    public int gearFixing();
+
+
+
+
+}
